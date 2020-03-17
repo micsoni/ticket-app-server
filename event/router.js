b4d26be05be06c65176d64ba2353e67b7a7dcf14
micsoni <micsoni@gmail.com>
2020-03-17 15:08:42 +0100

    filtered events that already happend and added pagination on endpoint get /event

--- event/router.js
+++ event/router.js
@@ -4,10 +4,17 @@ const Event = require("./model");
 const auth = require("../auth/middleware");
 const router = express.Router();
 const User = require("../user/model");
+const { Op } = require("sequelize");
+
+
 router.get("/event", async (req, res, next) => {
+  const limit = Math.min(req.query.limit || 9, 500)
+  const offset = req.query.offset || 0
   try {
-    const showEvent = await Event.findAll();
-    res.send(showEvent);
+    const showEvent = await Event.findAndCountAll({ where: {endDate:{[Op.gt]:new Date}},
+      limit, offset
+    });
+    res.send(showEvent.rows);
   } catch (error) {
     next(error);
   }
